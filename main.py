4ca97d1939e55d97542cc2ced19d9103253531b9
Rebecca-123 <rebecca.notw@gmail.com>
2021-12-07 15:23:39 -0800

    Reorganize navbar and pages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask, render_template
+
 # create an instance of flask object
 app = Flask(__name__)
 
@@ -11,9 +12,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/abouttheexam")
-def abouttheexam():
-    return render_template("abouttheexam.html")
+@app.route("/about-the-exam")
+def about_the_exam():
+    return render_template("about_the_exam.html")
 
 
 @app.route("/allie")
@@ -29,6 +30,7 @@ def gabriel():
 @app.route("/justin")
 def justin():
     return render_template("justin.html")
+
 
 @app.route("/tianbin")
 def tianbin():
@@ -53,65 +55,20 @@ def rebecca():
     return render_template("rebecca.html", players=players)
 
 
-@app.route("/Collaboration1")
-def Collaboration1():
-    return render_template("Big Idea/Big Idea 1/Collaboration1.html")
+@app.route("/collaboration")
+def collaboration():
+    return render_template("Big Idea/Big Idea 1/collaboration.html")
 
-@app.route("/Collaboration2")
-def Collaboration2():
-    return render_template("Big Idea/Big Idea 2/Collaboration2.html")
 
-@app.route("/Collaboration3")
-def Collaboration3():
-    return render_template("Big Idea/Big Idea 3/Collaboration3.html")
+@app.route("/creative-development")
+def creative_development():
+    return render_template("Big Idea/Big Idea 1/creative_development.html")
 
-@app.route("/Collaboration4")
-def Collaboration4():
-    return render_template("Big Idea/Big Idea 4/Collaboration4.html")
 
-@app.route("/Collaboration5")
-def Collaboration5():
-    return render_template("Big Idea/Big Idea 5/Collaboration5.html")
+@app.route("/program-design")
+def program_design():
+    return render_template("Big Idea/Big Idea 1/program_design.html")
 
-@app.route("/Creativedevelopment1")
-def Creativedevelopment1():
-    return render_template("Big Idea/Big Idea 1/Creativedevelopment1.html")
-
-@app.route("/Creativedevelopment2")
-def Creativedevelopment2():
-    return render_template("Big Idea/Big Idea 2/Creativedevelopment2.html")
-
-@app.route("/Creativedevelopment3")
-def Creativedevelopment3():
-    return render_template("Big Idea/Big Idea 3/Creativedevelopment3.html")
-
-@app.route("/Creativedevelopment4")
-def Creativedevelopment4():
-    return render_template("Big Idea/Big Idea 4/Creativedevelopment4.html")
-
-@app.route("/Creativedevelopment5")
-def Creativedevelopment5():
-    return render_template("Big Idea/Big Idea 5/Creativedevelopment5.html")
-
-@app.route("/Programdesign1")
-def Programdesign1():
-    return render_template("Big Idea/Big Idea 1/Programdesign1.html")
-
-@app.route("/Programdesign2")
-def Programdesign2():
-    return render_template("Big Idea/Big Idea 2/Programdesign2.html")
-
-@app.route("/Programdesign3")
-def Programdesign3():
-    return render_template("Big Idea/Big Idea 3/Programdesign3.html")
-
-@app.route("/Programdesign4")
-def Programdesign4():
-    return render_template("Big Idea/Big Idea 4/Programdesign4.html")
-
-@app.route("/Programdesign5")
-def Programdesign5():
-    return render_template("Big Idea/Big Idea 5/Programdesign5.html")
 
 @app.route("/support")
 def support():
